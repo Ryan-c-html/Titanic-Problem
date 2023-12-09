4997d439c73663db2932194b8db28842409cbf4b
Ryan <ryansalesj4@gmail.com>
2023-12-09 20:52:23 -0300

    Co-authored-by: João Gabriel Reis <jg01@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+# Importando bibliotecas
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('./datasets/train.csv', index_col=0)
+# Importando os arquivos .csv
+train = pd.read_csv('./datasets/train.csv', index_col=0)
+test = pd.read_csv('./datasets/test.csv', index_col=0)
 
-print(df)
+# Adquirindo informações sobre o número de linhas e colunas
+train.isnull().sum()
+test.isnull().sum()
+print("Train Shape:",train.shape)
+print("Test Shape:",test.shape)
+
+# Pegando informações sobre o dataFrame
+train.info()
+test.info()
+
+def analise_sobreviventes():
+    sobrevivente = (train['Survived']==1).value_counts()
+    return sobrevivente
+
+print(f"Sobreviventes: ", analise_sobreviventes())
